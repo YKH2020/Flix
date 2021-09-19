8896e9c667995e0bad41b5c8668b7c20f7f133ad
YKH2020 <yash.hegde.2020@gmail.com>
2021-09-19 11:11:06 -0400

    Part 2 Completed

---
+++ Flix/MovieGridCellCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCellCollectionViewCell.swift
+//  Flix
+//
+//  Created by Yash Hegde on 9/19/21.
+//
+
+import UIKit
+
+class MovieGridCellCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/ViewController.swift
+++ Flix/ViewController.swift
@@ -69,6 +69,19 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         
         return cell
     }
-
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        print("Loading up the details screen")
+        
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        let detailsViewController = segue.destination as! MovieDetailsViewController
+        
+        detailsViewController.movie = movie
+        
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
 
